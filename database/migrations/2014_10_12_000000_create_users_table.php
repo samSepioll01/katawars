642dc0e597245f3dd64bfc2dbc626f92dac568a7
Manuel Jesús Fernández Torrejón <manueljesus.fernandez.torrejon@iesdonana.org>
2023-02-25 01:17:07 +0100

    Modificando migración del modelo User para que acepte el campo bio y los timestamps del softDelete. Se añade el trait softDelete al modelo.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('bio')->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
