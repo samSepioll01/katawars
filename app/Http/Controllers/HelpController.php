ddcbaddffeed86fe95d4b0844377d819edbcb9c1
Manuel Jesús Fernández Torrejón <manueljesus.fernandez.torrejon@iesdonana.org>
2023-02-26 05:15:14 +0100

    Añadiendo el modelo Help y su migración correspondiente. El modelo Help es el encargado de gestionar las ayudas que se muestran al usuario en la sección Help. Se añaden también el controller, request, seeder, factories, etc.

---
+++ app/Http/Controllers/HelpController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreHelpRequest;
+use App\Http\Requests\UpdateHelpRequest;
+use App\Models\Help;
+
+class HelpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreHelpRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreHelpRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Help  $help
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Help $help)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Help  $help
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Help $help)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateHelpRequest  $request
+     * @param  \App\Models\Help  $help
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateHelpRequest $request, Help $help)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Help  $help
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Help $help)
+    {
+        //
+    }
+}
