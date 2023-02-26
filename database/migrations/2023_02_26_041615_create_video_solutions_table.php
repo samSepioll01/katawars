2d7c2459522e4659e90be55cfa4a4242c539c91c
Manuel Jesús Fernández Torrejón <manueljesus.fernandez.torrejon@iesdonana.org>
2023-02-26 05:21:29 +0100

    Añadiendo el modelo VideoSolution y su migración correspondiente. El modelo se encargará de gestionar los video para las soluciones en los retos cuando el usuario desbloquee el reto sin superarlo. Se incluyen también el controller, request, seeder, factories, policies.

---
+++ database/migrations/2023_02_26_041615_create_video_solutions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_solutions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('youtube_code');
+            $table->foreignId('kata_id')->constrained()
+                ->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_solutions');
+    }
+};
