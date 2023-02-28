d6436b55fcde458188f682a4f8c295157f22736e
Manuel Jesús Fernández Torrejón <manueljesus.fernandez.torrejon@iesdonana.org>
2023-02-28 04:31:00 +0100

    Implementando las relaciones entre los modelos Kata y Profile. Establecen el caso de uso cuando un usuario se salta un reto y ve las soluciones porque no puede superarlo.

--- app/Models/Kata.php
+++ app/Models/Kata.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Kata extends Model
 {
     use HasFactory;
+
+    /**
+     * Set the relationship with Profile model.
+     * This determines which profiles skipped a Kata.
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongToMany
+     */
+    public function skippedByProfiles(): BelongsToMany
+    {
+        return $this->belongsToMany(Profile::class, 'skipped_katas');
+    }
 }
--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Profile extends Model
 {
     use HasFactory;
+
+    /**
+     * Set the the relationship with Kata model.
+     * This determines which katas were skipped by the users
+     * and see their solutions.
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongToMany
+     */
+    public function skippedKatas(): BelongsToMany
+    {
+        return $this->belongsToMany(Kata::class, 'skipped_katas');
+    }
 }
