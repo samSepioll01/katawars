14fdf7648a8f3380822a65bcaf86a495cd294884
Manuel Jesús Fernández Torrejón <manueljesus.fernandez.torrejon@iesdonana.org>
2023-02-28 02:02:00 +0100

    Añadiendo la migración de la tabla pivote entre los modelos Profile y KataWay. Que establecen las Sendas de Katas a las que el usuario puede añadir para completar el conjunto de retos asociado a ellas.

---
+++ database/migrations/2023_02_27_234741_create_kataways_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kataway_profile', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kataway_id')->constrained('kata_ways', 'id')
+                ->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('profile_id')->constrained()
+                ->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamp('start_date');
+            $table->timestamp('end_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kataway_profile');
+    }
+};
