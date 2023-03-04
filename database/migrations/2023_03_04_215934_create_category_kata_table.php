1b71d5bd097378afa43067068118faafa59da90b
Manuel Jesús Fernández Torrejón <manueljesus.fernandez.torrejon@iesdonana.org>
2023-03-04 23:19:03 +0100

    Añadiendo restricción unique a la migración para evitar que una categoría pueda ser asignada dos veces al mismo kata.

--- database/migrations/2023_03_04_215934_create_category_kata_table.php
+++ database/migrations/2023_03_04_215934_create_category_kata_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->foreignId('kata_id')->constrained()
                 ->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
+            $table->unique(['category_id', 'kata_id']);
         });
     }
 
