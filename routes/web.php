806241fe12b4d0cc7173fb8d4bf9bc7ef0d8ae77
Manuel Jesús Fernández Torrejón <manueljesus.fernandez.torrejon@iesdonana.org>
2023-03-27 00:54:51 +0200

    Modificando variable global HOME para establecer la ruta hacia el dashboard del usuario. Se modifica y se añaden estilos iniciales al dashboard.

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
+    Route::get('/user/dashboard', function () {
+        return view('profile.dashboard');
     })->name('dashboard');
 });
