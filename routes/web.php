fc198a96c5bd1cbd01621be1a067d5754e340bde
Manuel Jesús Fernández Torrejón <manueljesus.fernandez.torrejon@iesdonana.org>
2023-04-02 01:49:39 +0200

    Añadiendo rutas para el controlador del cambio de tema.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\appThemeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', [appThemeController::class, 'initialConfig'])->name('home');
+Route::post('/save-theme', [appThemeController::class, 'saveModifiedTheme'])
+    ->name('save-theme');
+
 Route::get('/privacy-policy', fn() => view('policy'))->name('privacy-policy');
 Route::get('/terms-of-service', fn() => view('terms'))->name('terms-service');
 
@@ -22,7 +26,10 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/user/dashboard', function () {
-        return view('profile.dashboard');
-    })->name('dashboard');
+    Route::get('/user/dashboard', [appThemeController::class, 'authUserConfig'])
+        ->name('dashboard');
+
+    Route::get('/user', function() {
+        return redirect()->route('dashboard');
+    });
 });
