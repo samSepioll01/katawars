b45bccd053bd4022f4cd66b9b93603bc63419b7f
Manuel Jesús Fernández Torrejón <manueljesus.fernandez.torrejon@iesdonana.org>
2023-05-10 07:29:23 +0200

    Añadiendo última actividad a los metadatos para actualizar automáticamente cada vez que se renderiza el app layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="last_activity" content="{{ auth()->user()->sessions->first()->last_activity }}">
 
         <title>{{ config('app.name', 'KataWars') }}</title>
 
